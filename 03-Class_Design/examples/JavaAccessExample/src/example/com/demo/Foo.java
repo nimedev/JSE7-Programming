0179cd9d7f1d2721fea0502e4fd5bd613751258c
nicolas mejia <nicolasmejiaco@gmail.com>
2015-03-11 18:37:49 -0500

    Se arreglaron las carpetas de las practicas 2 y 3

--- 03-Class_Design/examples/JavaAccessExample/src/example/com/demo/Foo.java
+++
@@ -1,6 +0,0 @@
-package example.com.demo;
-
-public class Foo {
-
-    protected int result;
-}
--- 03-Class_Design/solutions/practice1/EmployeeSolution/src/com/example/domain/Admin.java
+++
@@ -1,8 +0,0 @@
-package com.example.domain;
-
-public class Admin extends Employee {
-
-    public Admin(int empId, String name, String ssn, double salary) {
-        super(empId, name, ssn, salary);
-    }
-}
